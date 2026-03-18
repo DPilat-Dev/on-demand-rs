f540782dd64dc19c616115e546d494978e58af6e
DPilat-Dev <david.j.pilat2gmail.com>
2026-03-17 18:18:05 -0700

    Add vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -36,6 +37,7 @@ export default function RootLayout({
         </noscript>
         {/* End Google Tag Manager (noscript) */}
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
